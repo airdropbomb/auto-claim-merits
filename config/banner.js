96dc7c306744766740307dd976a4cd6dca8c791b
airdropbomb <saginaw135@gmail.com>
2025-03-28 21:07:57 +0630

    Update banner.js

--- config/banner.js
+++ config/banner.js
@@ -1,16 +1,19 @@
 const figlet = require('figlet');
 const chalk = require('chalk').default;
 
-function displayBanner(){
-    const banner = figlet.textSync ('BlockScout', {
-        font: 'ANSI Shadow',
-        horizontalLayout: 'default',
-        verticalLayout: 'default'
-    })
-    console.log(chalk.green(banner))
+function displayBanner() {
+    const banner = `
+       █████╗ ██████╗ ██████╗     ███╗   ██╗ ██████╗ ██████╗ ███████╗
+      ██╔══██╗██╔══██╗██╔══██╗    ████╗  ██║██╔═══██╗██╔══██╗██╔════╝
+      ███████║██║  ██║██████╔╝    ██╔██╗ ██║██║   ██║██║  ██║█████╗  
+      ██╔══██║██║  ██║██╔══██╗    ██║╚██╗██║██║   ██║██║  ██║██╔══╝  
+      ██║  ██║██████╔╝██████╔╝    ██║ ╚████║╚██████╔╝██████╔╝███████╗
+      ╚═╝  ╚═╝╚═════╝ ╚═════╝     ╚═╝  ╚═══╝ ╚═════╝ ╚═════╝ ╚══════╝
+    `;
+    console.log(chalk.green(banner));
     console.log(chalk.yellow('========================================='));
-    console.log(chalk.magenta('Github   : https://github.com/0x-Disciple'));
-    console.log(chalk.magenta('Telegram : https://t.me/CryptoKidzs'));
-    console.log(chalk.yellow('========================================='))
+    console.log(chalk.magenta('Telegram : https://t.me/airdropbombnode'));
+    console.log(chalk.magenta('Youtube   : https://youtube.com/@airdropbombnode'));
+    console.log(chalk.yellow('========================================='));
 }
 displayBanner();
